b430cf1191c343dd5a0b344028d73e23cde9881b
Joseph 1864 <josephwilliamsm99@gmail.com>
2023-07-04 15:00:19 -0400

    New evaluateExpression method, modified equalsPressed to catch ArithmeticExceptions

--- app/src/main/java/com/joew/calculator/Calculator.kt
+++ app/src/main/java/com/joew/calculator/Calculator.kt
@@ -1,60 +1,29 @@
 package com.joew.calculator
 
+import java.math.BigDecimal
+import java.math.MathContext
+
 class Calculator {
 
-    fun evaluateExpression(expression: String) : Double {
-        if (expression.isEmpty()) return 0.0
-        var total: Double = 0.0
-        var operand1: String = ""
-        var operand2: String = ""
-        var operation: Int = 0
-        var breakRecursion: Boolean = false
+    private val mathContext = MathContext(10)
 
-        for (i in expression.indices) {
-            operand1 += expression[i]
-            if (operation == 0 && i > 0 && (expression[i] == '+' || expression[i] == '-' || expression[i] == 'X' || expression[i] == '/')) {
-                when(expression[i]) {
-                    '+' -> operation = 1
-                    '-' -> operation = 2
-                    'X' -> operation = 3
-                    '/' -> operation = 4
-                }
-                operand2 = operand1.dropLast(1)
-                operand1 = ""
-            } else if (operation == 0 && i == expression.length -1) {
-                total += operand1.toDouble()
-            } else if (operation != 0 && i == expression.length - 1) {
-                when(operation) {
-                    1 -> total = operand2.toDouble() + operand1.toDouble()
-                    2 -> total = operand2.toDouble() - operand1.toDouble()
-                    3 -> total = operand2.toDouble() * operand1.toDouble()
-                    4 -> total = operand2.toDouble() / operand1.toDouble()
-                }
-            } else if (operation != 0 && (expression[i] == '+' || expression[i] == '-')) {
-                when(operation) {
-                    1 -> total = operand2.toDouble() + operand1.dropLast(1).toDouble()
-                    2 -> total = operand2.toDouble() - operand1.dropLast(1).toDouble()
-                    3 -> total = operand2.toDouble() * operand1.dropLast(1).toDouble()
-                    4 -> total = operand2.toDouble() / operand1.dropLast(1).toDouble()
-                }
-                if (expression[i] == '+') {
-                    operation = 1
-                } else {
-                    operation = 2
-                }
-                operand2 = total.toString()
-                operand1 = ""
-            } else if (operation != 0 && (expression[i] == 'X' || expression[i] == '/')) {
-                when(operation) {
-                    1 -> total += operand2.toDouble() + evaluateExpression(expression.drop(i - operand1.length))
-                    2 -> total += operand2.toDouble() - evaluateExpression(expression.drop(i - operand1.length))
-                    3 -> total += operand2.toDouble() * evaluateExpression(expression.drop(i - operand1.length))
-                    4 -> total += operand2.toDouble() / evaluateExpression(expression.drop(i - operand1.length))
-                }
-                breakRecursion = true
-            }
-            if (breakRecursion) break
+    fun evaluateExpression(expression: String) : BigDecimal  = when {
+        expression.contains('+') -> with(expression) {
+            evaluateExpression(substringBefore('+'))
+                .plus(evaluateExpression(substringAfter('+')))
         }
-        return total
+        expression.contains('-') -> with(expression) {
+            evaluateExpression(substringBefore('-'))
+                .minus(evaluateExpression(substringAfter('-')))
+        }
+        expression.contains('X') -> with(expression) {
+            evaluateExpression(substringBefore('X'))
+                .multiply(evaluateExpression(substringAfter('X')), mathContext)
+        }
+        expression.contains('/') -> with(expression) {
+            evaluateExpression(substringBefore('/'))
+                .divide(evaluateExpression(substringAfter('/')), mathContext)
+        }
+        else -> expression.trim().toBigDecimal()
     }
 }
--- app/src/main/java/com/joew/calculator/screens/MainScreenViewModel.kt
+++ app/src/main/java/com/joew/calculator/screens/MainScreenViewModel.kt
@@ -9,6 +9,7 @@ import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
+import java.lang.ArithmeticException
 import java.text.DecimalFormat
 
 class MainScreenViewModel: ViewModel() {
@@ -18,7 +19,6 @@ class MainScreenViewModel: ViewModel() {
     val calculator = Calculator()
 
     fun buttonPressed(input: Char) {
-
         viewModelScope.launch {
             _uiState.update {
                 it.copy(
@@ -29,14 +29,21 @@ class MainScreenViewModel: ViewModel() {
     }
 
     fun equalsPressed() {
-        val decimalFormat = DecimalFormat("#.###")
+        try {
+            updateView()
+        } catch(e: ArithmeticException) {
+            undefinedInput()
+        }
+    }
 
-        viewModelScope.launch {
-            _uiState.update {
-                it.copy(
-                    result = decimalFormat.format(calculator.evaluateExpression(_uiState.value.totalExpression)).toString()
-                )
-            }
+    fun updateView()  {
+        DecimalFormat("#.#####").format(calculator.evaluateExpression(_uiState.value.totalExpression))
+    }
+    fun undefinedInput() {
+        _uiState.update {
+            it.copy(
+                result = "Undefined"
+            )
         }
     }
 
@@ -60,6 +67,6 @@ class MainScreenViewModel: ViewModel() {
 }
 
 data class MainScreenViewState(
-    var totalExpression: String = "",
+    val totalExpression: String = "",
     val result: String = ""
 )
